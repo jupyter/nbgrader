b07769efa9c8a71a954e5b2d3e61c409e0cdacce
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-09-15 23:43:02 -0700

    Update example notebooks

--- examples/nbgrader_assign_config.py
+++ examples/nbgrader_assign_config.py
@@ -3,4 +3,3 @@ c = get_config()
 c.AssignApp.notebooks = ['Assignment Template.ipynb']
 c.AssignApp.output_base = 'Assignment'
 c.IncludeHeaderFooter.header = "header.ipynb"
-c.RenderSolutions.title = "Example Assignment"
--- examples/nbgrader_solution_config.py
+++
@@ -1,6 +0,0 @@
-c = get_config()
-
-c.SolutionApp.notebooks = ['Assignment Template.ipynb']
-c.SolutionApp.output_base = 'Assignment Solution'
-c.IncludeHeaderFooter.header = "header.ipynb"
-c.RenderSolutions.title = "Example Assignment"
