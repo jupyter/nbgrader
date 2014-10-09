5359c3a90ee22e4aa60d506de194c8b7413b9d3d
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-08 21:29:48 -0700

    Use fabric instead of make

--- docs/user_guide/ipython_nbconvert_config.py
+++
@@ -1,7 +0,0 @@
-c = get_config()
-
-c.NbConvertApp.export_format = 'notebook'
-c.NbConvertApp.notebooks = ["*.ipynb"]
-c.Exporter.preprocessors = ['IPython.nbconvert.preprocessors.ExecutePreprocessor']
-
-
