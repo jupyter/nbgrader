64feb2ed638e43f15b7008507907f7d607ebccf3
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-10-31 15:01:30 -0700

    Add some flags to nbgrader assign

--- nbgrader/apps/assignapp.py
+++ nbgrader/apps/assignapp.py
@@ -1,12 +1,29 @@
 from IPython.config.loader import Config
-from IPython.config.application import catch_config_error
 from IPython.utils.traitlets import Unicode
 from nbgrader.apps.customnbconvertapp import CustomNbConvertApp
+from nbgrader.apps.customnbconvertapp import aliases as base_aliases
+from nbgrader.apps.customnbconvertapp import flags as base_flags
+
+
+aliases = {}
+aliases.update(base_aliases)
+aliases.update({
+    'header': 'IncludeHeaderFooter.header',
+    'footer': 'IncludeHeaderFooter.footer'
+})
+
+flags = {}
+flags.update(base_flags)
+flags.update({
+})
+
 
 class AssignApp(CustomNbConvertApp):
-    
+
     name = Unicode(u'nbgrader-assign')
     description = Unicode(u'Prepare a student version of an assignment by removing solutions')
+    aliases = aliases
+    flags = flags
 
     def _export_format_default(self):
         return 'notebook'
