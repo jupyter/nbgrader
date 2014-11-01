7f2b2671cf03713848679e4b12ad987621267166
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-11-01 15:49:52 -0700

    Update docs

--- docs/user_guide/grade_example/nbgrader_autograde_config.py
+++ docs/user_guide/grade_example/nbgrader_autograde_config.py
@@ -1,8 +1,9 @@
 c = get_config()
 
 import os
-c.AutogradeApp.notebooks = ['submitted/*.ipynb']
-c.FilesWriter.build_directory = os.path.join(os.getcwd(), 'autograded')
-c.FindStudentID.regexp = r".*/submitted/Problem [0-9] (?P<student_id>.+).ipynb"
+c.AutogradeApp.notebooks = ['submitted']
+c.AutogradeApp.output_dir = os.path.join(os.getcwd(), 'autograded')
+c.AutogradeApp.recursive = True
+c.FindStudentID.regexp = r"submitted/(?P<student_id>.+)/.*.ipynb"
 c.SaveAutoGrades.assignment_id = "Problem Set 1"
 c.SaveAutoGrades.db_name = "example"
