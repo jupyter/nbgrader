980c6f5a52e1ed06a3e6705609f5d157f103271e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-01-19 16:32:48 -0800

    Add preprocess to check grade ids

---
+++ nbgrader/preprocessors/checkgradeids.py
@@ -0,0 +1,25 @@
+from IPython.nbconvert.preprocessors import Preprocessor
+from nbgrader import utils
+
+class CheckGradeIds(Preprocessor):
+    """A preprocessor for checking that grade ids are unique."""
+
+    def preprocess(self, nb, resources):
+        resources['grade_ids'] = ids = []
+        nb, resources = super(CheckGradeIds, self).preprocess(nb, resources)
+
+        id_set = set([])
+        for grade_id in ids:
+            if grade_id in id_set:
+                raise RuntimeError("Duplicate grade id: {}".format(grade_id))
+            id_set.add(grade_id)
+
+        return nb, resources
+
+    def preprocess_cell(self, cell, resources, cell_index):
+        if utils.is_grade(cell):
+            grade_id = cell.metadata.nbgrader.get("grade_id", "")
+            if grade_id == "":
+                raise RuntimeError("Blank grade id!")
+            resources['grade_ids'].append(grade_id)
+        return cell, resources
