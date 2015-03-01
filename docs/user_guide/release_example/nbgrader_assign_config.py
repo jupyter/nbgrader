8ec5fdec3c830ca0ef8863a9662e669bbda2e382
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-02-28 18:41:51 -0800

    Update example nbgrader_assign_config.py in docs

--- docs/user_guide/release_example/nbgrader_assign_config.py
+++ docs/user_guide/release_example/nbgrader_assign_config.py
@@ -1,11 +1,8 @@
 c = get_config()
 
-import os
-cwd = os.getcwd()
-
 c.AssignApp.notebooks = ['teacher/*.ipynb']
-c.AssignApp.output_dir = os.path.join(cwd, 'student')
-c.IncludeHeaderFooter.header = os.path.join(cwd, 'header.ipynb')
+c.FilesWriter.build_directory = 'student'
+c.IncludeHeaderFooter.header = 'header.ipynb'
 
 # These are only used if run with --save-cells
 c.SaveGradeCells.assignment_id = "Problem Set 1"
