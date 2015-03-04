31dae2718207db8ee6539cede73fb443065eef1c
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-04 10:58:19 -0800

    Simpler doc building commands

--- docs/fabfile.py
+++ docs/fabfile.py
@@ -4,15 +4,11 @@ import glob
 
 def build():
     os.chdir('command_line_tools')
-    for filename in glob.glob("*.ipynb"):
-        basename = os.path.splitext(filename)[0]
-        local('ipython nbconvert --config=../ipython_nbconvert_config.py --profile-dir=/tmp --output "{}" "{}"'.format(basename, filename))
+    local('ipython nbconvert --to notebook --execute --inplace --profile-dir=/tmp *.ipynb')
     os.chdir('..')
 
     os.chdir('user_guide')
-    for filename in glob.glob("*.ipynb"):
-        basename = os.path.splitext(filename)[0]
-        local('ipython nbconvert --config=../ipython_nbconvert_config.py --profile-dir=/tmp --output "{}" "{}"'.format(basename, filename))
+    local('ipython nbconvert --to notebook --execute --inplace --profile-dir=/tmp *.ipynb')
     os.chdir('..')
 
 
