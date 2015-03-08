91f5d6a8eef58dbd48c7f5c4965058fa3e17279e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-07 17:14:28 -0800

    Update ComputeChecksums preprocessor and improve tests

--- nbgrader/preprocessors/computechecksums.py
+++ nbgrader/preprocessors/computechecksums.py
@@ -4,15 +4,27 @@ from .. import utils
 class ComputeChecksums(Preprocessor):
     """A preprocessor to compute checksums of grade cells."""
 
+    def preprocess(self, nb, resources):
+        self.comment_index = 0
+        nb, resources = super(ComputeChecksums, self).preprocess(nb, resources)
+        return nb, resources
+
     def preprocess_cell(self, cell, resources, cell_index):
-        # compute checksums of grade cells, but not those of solution
-        # cells, because solution cells will be modified by students
-        if utils.is_grade(cell) and not utils.is_solution(cell):
+        # compute checksums of grade cell and solution cells
+        if utils.is_grade(cell) or utils.is_solution(cell):
             checksum = utils.compute_checksum(cell)
             cell.metadata.nbgrader['checksum'] = checksum
-            self.log.debug(
-                "Checksum for '%s' is %s",
-                cell.metadata.nbgrader['grade_id'],
-                checksum)
+
+            if utils.is_grade(cell):
+                self.log.debug(
+                    "Checksum for '%s' is %s",
+                    cell.metadata.nbgrader['grade_id'],
+                    checksum)
+            if utils.is_solution(cell):
+                self.log.debug(
+                    "Checksum for solution cell #%s is %s",
+                    self.comment_index,
+                    checksum)
+                self.comment_index += 1
 
         return cell, resources
