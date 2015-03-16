e2d598ed7fe306d34389fe18e3c9f026f409f4f8
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-15 18:24:48 -0700

    Fix imports

--- nbgrader/apps/__init__.py
+++ nbgrader/apps/__init__.py
@@ -1,12 +1,12 @@
-from baseapp import BaseNbGraderApp, BaseNbConvertApp
-from nbgraderapp import NbGraderApp
-from assignapp import AssignApp
-from autogradeapp import AutogradeApp
-from feedbackapp import FeedbackApp
-from notebookapp import FormgradeNotebookApp
-from formgradeapp import FormgradeApp
-from submitapp import SubmitApp
-from validateapp import ValidateApp
+from .baseapp import BaseNbGraderApp, BaseNbConvertApp
+from .nbgraderapp import NbGraderApp
+from .assignapp import AssignApp
+from .autogradeapp import AutogradeApp
+from .feedbackapp import FeedbackApp
+from .notebookapp import FormgradeNotebookApp
+from .formgradeapp import FormgradeApp
+from .submitapp import SubmitApp
+from .validateapp import ValidateApp
 
 __all__ = [
     'BaseNbGraderApp',
