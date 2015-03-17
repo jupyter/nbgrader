88268940691115e6a7a31a981da63c8b419d946c
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-17 12:01:02 -0700

    Print out the coverage env variable

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,3 +1,5 @@
+import os
+print("COVERAGE_PROCESS_START={}".format(os.environ.get('COVERAGE_PROCESS_START', '')))
 # import coverage for when testing subprocesses
 import coverage
 coverage.process_startup()
