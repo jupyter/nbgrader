639824dfa86b2aa98b1ae2ca3d4a5cec6ca329ea
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-18 17:28:33 -0700

    Remove FindStudentID from preprocessors init

--- nbgrader/preprocessors/__init__.py
+++ nbgrader/preprocessors/__init__.py
@@ -1,7 +1,6 @@
 from .headerfooter import IncludeHeaderFooter
 from .lockcells import LockCells
 from .clearsolutions import ClearSolutions
-from .findstudentid import FindStudentID
 from .saveautogrades import SaveAutoGrades
 from .displayautogrades import DisplayAutoGrades
 from .computechecksums import ComputeChecksums
