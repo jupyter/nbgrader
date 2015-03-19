25e88ab3230823c03897bd390dc76b1b153fff72
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-18 20:01:03 -0700

    Remove usercustomize.py, and just add customizations in travis

--- usercustomize.py
+++
@@ -1,5 +0,0 @@
-import os
-print("COVERAGE_PROCESS_START={}".format(os.environ.get('COVERAGE_PROCESS_START', '')))
-# import coverage for when testing subprocesses
-import coverage
-coverage.process_startup()
