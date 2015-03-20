025a07623f0527b55aa30da291330e48e173c47e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-20 13:36:38 -0700

    Fix nbgrader_formgrade_config

--- docs/user_guide/grade_example/nbgrader_formgrade_config.py
+++ docs/user_guide/grade_example/nbgrader_formgrade_config.py
@@ -1,6 +1,5 @@
 c = get_config()
 
 c.FormgradeApp.base_directory = "autograded"
-c.FormgradeApp.db_name = "example"
 c.FormgradeApp.directory_format = "{student_id}/{notebook_id}.ipynb"
-c.FormgradeApp.db_url = "sqlite:////tmp/nbgrader_example.db"
+c.FormgradeApp.db_url = "sqlite:////../nbgrader_example.db"
