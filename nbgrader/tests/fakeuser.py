f9f47a1ea6dbb25191d62f2511c0d493cda1d013
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-03-24 14:02:11 -0700

    Add hubauth tests

---
+++ nbgrader/tests/fakeuser.py
@@ -0,0 +1,33 @@
+import os
+
+from jupyterhub.auth import LocalAuthenticator
+from jupyterhub.spawner import LocalProcessSpawner
+from tornado import gen
+
+class FakeUserAuth(LocalAuthenticator):
+    """Authenticate fake users"""
+
+    @gen.coroutine
+    def authenticate(self, handler, data):
+        """If the user is on the whitelist, authenticate regardless of password.
+        If not, then don't authenticate.
+
+        """
+        username = data['username']
+        if not self.check_whitelist(username):
+            return
+        return username
+
+
+class FakeUserSpawner(LocalProcessSpawner):
+
+    def user_env(self, env):
+        env['USER'] = self.user.name
+        env['HOME'] = os.getcwd()
+        env['SHELL'] = '/bin/bash'
+        return env
+
+    def make_preexec_fn(self, name):
+        def preexec():
+            return
+        return preexec
