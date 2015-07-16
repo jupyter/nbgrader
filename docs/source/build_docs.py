c2388e67dda126ae46713d1517d7d873ce9b453d
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-07-16 13:21:57 -0700

    Install special nbgrader kernelspec

--- docs/source/build_docs.py
+++ docs/source/build_docs.py
@@ -4,14 +4,21 @@ import re
 import shutil
 import sys
 import nbgrader.apps
+import json
 
 from textwrap import dedent
+from copy import deepcopy
 from clear_docs import run, clear_notebooks
 
 try:
     from StringIO import StringIO # Python 2
 except ImportError:
     from io import StringIO # Python 3
+
+try:
+    from IPython.nbformat import read, write
+except ImportError:
+    print("Warning: IPython could not be imported, some tasks may not work")
 
 
 # get absolute path to IPython to make sure it's the same one as what is installed
@@ -90,6 +97,75 @@ def autogen_config(root):
         f.write(config)
 
 
+def install_kernelspec():
+    print('Installing nbgrader kernelspec')
+    kernelspec = {
+        "language": "python",
+        "display_name": "nbgrader",
+        "argv": [
+            sys.executable,
+            "-m",
+            "IPython.kernel",
+            "-f",
+            "{connection_file}"
+        ]
+    }
+
+    ipython_dir = os.path.join(os.environ['HOME'], '.ipython')
+    kernelspec_dir = os.path.join(ipython_dir, 'kernels', 'nbgrader')
+    if not os.path.exists(kernelspec_dir):
+        os.makedirs(kernelspec_dir)
+    with open(os.path.join(kernelspec_dir, 'kernel.json'), 'w') as fh:
+        json.dump(kernelspec, fh, indent=1)
+
+
+def set_kernelspecs(root):
+    print('Setting kernelspecs to nbgrader')
+    modified = []
+    for dirname, dirnames, filenames in os.walk(root):
+        for filename in sorted(filenames):
+            if os.path.splitext(filename)[1] == '.ipynb':
+                # read in the notebook
+                pth = os.path.join(dirname, filename)
+                modified.append(pth)
+                with open(pth, 'r') as fh:
+                    orig_nb = read(fh, 4)
+
+                # copy the original notebook
+                new_nb = deepcopy(orig_nb)
+
+                # set kernelspec
+                new_nb.metadata = {
+                    "kernelspec": {
+                        "display_name": "nbgrader",
+                        "language": "python",
+                        "name": "nbgrader"
+                    }
+                }
+
+                # write the notebook back to disk
+                with open(pth, 'w') as fh:
+                    write(new_nb, fh, 4)
+
+    return modified
+
+
+def unset_kernelspecs(root, modified):
+    print('Unsetting kernelspecs')
+    for pth in modified:
+        with open(pth, 'r') as fh:
+            orig_nb = read(fh, 4)
+
+        # copy the original notebook
+        new_nb = deepcopy(orig_nb)
+
+        # unset kernelspec
+        new_nb.metadata = {}
+
+        # write the notebook back to disk
+        with open(pth, 'w') as fh:
+            write(new_nb, fh, 4)
+
 
 def build_notebooks(root):
     """Execute notebooks and convert them to rst"""
@@ -147,7 +223,13 @@ def build_notebooks(root):
 
 if __name__ == "__main__":
     root = os.path.abspath(os.path.dirname(__file__))
+
     clear_notebooks(root)
+
+    install_kernelspec()
+    modified = set_kernelspecs(root)
     build_notebooks(root)
+    unset_kernelspecs(root, modified)
+
     autogen_command_line(root)
     autogen_config(root)
