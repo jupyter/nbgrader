c0169c5073e4a83120f4d6860258c3085b4c1cf5
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-07-16 15:01:47 -0700

    Use flit that's been installed in the virtualenv

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import subprocess as sp
+import sys
+import os
 
 print('Warning: this setup.py uses flit, not setuptools.')
 print('Behavior may not be exactly what you expect. Use at your own risk!')
 
-sp.check_call(['flit', 'install', '--deps', 'production'])
+flit = os.path.join(os.path.dirname(sys.executable), 'flit')
+cmd = [flit, 'install', '--deps', 'production']
+print(" ".join(cmd))
+sp.check_call(cmd)
