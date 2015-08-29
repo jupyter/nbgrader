7b1eedb69a54d1e50efb3a28fe5670037c8a9f28
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-08-29 16:20:20 -0700

    Update tests for nbgrader submit

--- nbgrader/tests/apps/test_nbgrader_submit.py
+++ nbgrader/tests/apps/test_nbgrader_submit.py
@@ -2,6 +2,8 @@ import os
 import datetime
 import time
 
+from os.path import join, isfile
+
 from ...utils import parse_utc
 from .. import run_python_module
 from .base import BaseTestApp
@@ -9,8 +11,8 @@ from .base import BaseTestApp
 
 class TestNbGraderSubmit(BaseTestApp):
 
-    def _release_and_fetch(self, assignment, exchange, cache):
-        self._copy_file("files/test.ipynb", "release/ps1/p1.ipynb")
+    def _release_and_fetch(self, assignment, exchange, cache, course_dir):
+        self._copy_file(join("files", "test.ipynb"), join(course_dir, "release", "ps1", "p1.ipynb"))
         run_python_module([
             "nbgrader", "release", assignment,
             "--course", "abc101",
@@ -41,9 +43,9 @@ class TestNbGraderSubmit(BaseTestApp):
         """Does the help display without error?"""
         run_python_module(["nbgrader", "submit", "--help-all"])
 
-    def test_no_course_id(self, exchange, cache):
+    def test_no_course_id(self, exchange, cache, course_dir):
         """Does releasing without a course id thrown an error?"""
-        self._release_and_fetch("ps1", exchange, cache)
+        self._release_and_fetch("ps1", exchange, cache, course_dir)
         cmd = [
             "nbgrader", "submit", "ps1",
             "--TransferApp.cache_directory={}".format(cache),
@@ -51,54 +53,54 @@ class TestNbGraderSubmit(BaseTestApp):
         ]
         run_python_module(cmd, retcode=1)
 
-    def test_submit(self, exchange, cache):
-        self._release_and_fetch("ps1", exchange, cache)
+    def test_submit(self, exchange, cache, course_dir):
+        self._release_and_fetch("ps1", exchange, cache, course_dir)
         now = datetime.datetime.now()
 
         time.sleep(1)
         self._submit("ps1", exchange, cache)
 
-        filename, = os.listdir(os.path.join(exchange, "abc101", "inbound"))
+        filename, = os.listdir(join(exchange, "abc101", "inbound"))
         username, assignment, timestamp1 = filename.split("+")
-        assert username == os.environ['USER']
+        assert username == os.environ["USER"]
         assert assignment == "ps1"
         assert parse_utc(timestamp1) > now
-        assert os.path.isfile(os.path.join(exchange, "abc101", "inbound", filename, "p1.ipynb"))
-        assert os.path.isfile(os.path.join(exchange, "abc101", "inbound", filename, "timestamp.txt"))
-        with open(os.path.join(exchange, "abc101", "inbound", filename, "timestamp.txt"), "r") as fh:
+        assert isfile(join(exchange, "abc101", "inbound", filename, "p1.ipynb"))
+        assert isfile(join(exchange, "abc101", "inbound", filename, "timestamp.txt"))
+        with open(join(exchange, "abc101", "inbound", filename, "timestamp.txt"), "r") as fh:
             assert fh.read() == timestamp1
 
-        filename, = os.listdir(os.path.join(cache, "abc101"))
+        filename, = os.listdir(join(cache, "abc101"))
         username, assignment, timestamp1 = filename.split("+")
-        assert username == os.environ['USER']
+        assert username == os.environ["USER"]
         assert assignment == "ps1"
         assert parse_utc(timestamp1) > now
-        assert os.path.isfile(os.path.join(cache, "abc101", filename, "p1.ipynb"))
-        assert os.path.isfile(os.path.join(cache, "abc101", filename, "timestamp.txt"))
-        with open(os.path.join(cache, "abc101", filename, "timestamp.txt"), "r") as fh:
+        assert isfile(join(cache, "abc101", filename, "p1.ipynb"))
+        assert isfile(join(cache, "abc101", filename, "timestamp.txt"))
+        with open(join(cache, "abc101", filename, "timestamp.txt"), "r") as fh:
             assert fh.read() == timestamp1
 
         time.sleep(1)
         self._submit("ps1", exchange, cache)
 
-        assert len(os.listdir(os.path.join(exchange, "abc101", "inbound"))) == 2
-        filename = sorted(os.listdir(os.path.join(exchange, "abc101", "inbound")))[1]
+        assert len(os.listdir(join(exchange, "abc101", "inbound"))) == 2
+        filename = sorted(os.listdir(join(exchange, "abc101", "inbound")))[1]
         username, assignment, timestamp2 = filename.split("+")
-        assert username == os.environ['USER']
+        assert username == os.environ["USER"]
         assert assignment == "ps1"
         assert parse_utc(timestamp2) > parse_utc(timestamp1)
-        assert os.path.isfile(os.path.join(exchange, "abc101", "inbound", filename, "p1.ipynb"))
-        assert os.path.isfile(os.path.join(exchange, "abc101", "inbound", filename, "timestamp.txt"))
-        with open(os.path.join(exchange, "abc101", "inbound", filename, "timestamp.txt"), "r") as fh:
+        assert isfile(join(exchange, "abc101", "inbound", filename, "p1.ipynb"))
+        assert isfile(join(exchange, "abc101", "inbound", filename, "timestamp.txt"))
+        with open(join(exchange, "abc101", "inbound", filename, "timestamp.txt"), "r") as fh:
             assert fh.read() == timestamp2
 
-        assert len(os.listdir(os.path.join(cache, "abc101"))) == 2
-        filename = sorted(os.listdir(os.path.join(cache, "abc101")))[1]
+        assert len(os.listdir(join(cache, "abc101"))) == 2
+        filename = sorted(os.listdir(join(cache, "abc101")))[1]
         username, assignment, timestamp2 = filename.split("+")
-        assert username == os.environ['USER']
+        assert username == os.environ["USER"]
         assert assignment == "ps1"
         assert parse_utc(timestamp2) > parse_utc(timestamp1)
-        assert os.path.isfile(os.path.join(cache, "abc101", filename, "p1.ipynb"))
-        assert os.path.isfile(os.path.join(cache, "abc101", filename, "timestamp.txt"))
-        with open(os.path.join(cache, "abc101", filename, "timestamp.txt"), "r") as fh:
+        assert isfile(join(cache, "abc101", filename, "p1.ipynb"))
+        assert isfile(join(cache, "abc101", filename, "timestamp.txt"))
+        with open(join(cache, "abc101", filename, "timestamp.txt"), "r") as fh:
             assert fh.read() == timestamp2
