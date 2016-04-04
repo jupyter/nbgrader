d5a6b06977c0833be78518b9ec93a213a1a1684d
Jessica B. Hamrick <jhamrick@berkeley.edu>
2016-04-04 14:29:48 -0700

    Use splitlines function in nbgrader validate tests

--- nbgrader/tests/apps/test_nbgrader_validate.py
+++ nbgrader/tests/apps/test_nbgrader_validate.py
@@ -9,11 +9,6 @@ from .base import BaseTestApp
 
 class TestNbGraderValidate(BaseTestApp):
 
-    if sys.platform == 'win32':
-        newline = '\r\n'
-    else:
-        newline = '\n'
-
     def test_help(self):
         """Does the help display without error?"""
         run_nbgrader(["validate", "--help-all"])
@@ -22,31 +17,31 @@ class TestNbGraderValidate(BaseTestApp):
         """Does the validation fail on an unchanged notebook?"""
         self._copy_file(join("files", "submitted-unchanged.ipynb"), "submitted-unchanged.ipynb")
         output = run_nbgrader(["validate", "submitted-unchanged.ipynb"], stdout=True)
-        assert output.split(self.newline)[0] == "VALIDATION FAILED ON 3 CELL(S)! If you submit your assignment as it is, you WILL NOT"
+        assert output.splitlines()[0] == "VALIDATION FAILED ON 3 CELL(S)! If you submit your assignment as it is, you WILL NOT"
 
     def test_validate_changed(self):
         """Does the validation pass on an changed notebook?"""
         self._copy_file(join("files", "submitted-changed.ipynb"), "submitted-changed.ipynb")
         output = run_nbgrader(["validate", "submitted-changed.ipynb"], stdout=True)
-        assert output == "Success! Your notebook passes all the tests.{}".format(self.newline)
+        assert output == "Success! Your notebook passes all the tests.{}".formatlines()
 
     def test_invert_validate_unchanged(self):
         """Does the inverted validation pass on an unchanged notebook?"""
         self._copy_file(join("files", "submitted-unchanged.ipynb"), "submitted-unchanged.ipynb")
         output = run_nbgrader(["validate", "submitted-unchanged.ipynb", "--invert"], stdout=True)
-        assert output.split(self.newline)[0] == "NOTEBOOK PASSED ON 1 CELL(S)!"
+        assert output.splitlines()[0] == "NOTEBOOK PASSED ON 1 CELL(S)!"
 
     def test_invert_validate_changed(self):
         """Does the inverted validation fail on a changed notebook?"""
         self._copy_file(join("files", "submitted-changed.ipynb"), "submitted-changed.ipynb")
         output = run_nbgrader(["validate", "submitted-changed.ipynb", "--invert"], stdout=True)
-        assert output.split(self.newline)[0] == "NOTEBOOK PASSED ON 2 CELL(S)!"
+        assert output.splitlines()[0] == "NOTEBOOK PASSED ON 2 CELL(S)!"
 
     def test_grade_cell_changed(self):
         """Does the validate fail if a grade cell has changed?"""
         self._copy_file(join("files", "submitted-grade-cell-changed.ipynb"), "submitted-grade-cell-changed.ipynb")
         output = run_nbgrader(["validate", "submitted-grade-cell-changed.ipynb"], stdout=True)
-        assert output.split(self.newline)[0] == "THE CONTENTS OF 1 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
+        assert output.splitlines()[0] == "THE CONTENTS OF 1 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
 
     def test_grade_cell_changed_ignore_checksums(self):
         """Does the validate pass if a grade cell has changed but we're ignoring checksums?"""
@@ -55,13 +50,13 @@ class TestNbGraderValidate(BaseTestApp):
             "validate", "submitted-grade-cell-changed.ipynb",
             "--DisplayAutoGrades.ignore_checksums=True"
         ], stdout=True)
-        assert output.split(self.newline)[0] == "Success! Your notebook passes all the tests."
+        assert output.splitlines()[0] == "Success! Your notebook passes all the tests."
 
     def test_invert_grade_cell_changed(self):
         """Does the validate fail if a grade cell has changed, even with --invert?"""
         self._copy_file(join("files", "submitted-grade-cell-changed.ipynb"), "submitted-grade-cell-changed.ipynb")
         output = run_nbgrader(["validate", "submitted-grade-cell-changed.ipynb", "--invert"], stdout=True)
-        assert output.split(self.newline)[0] == "THE CONTENTS OF 1 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
+        assert output.splitlines()[0] == "THE CONTENTS OF 1 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
 
     def test_invert_grade_cell_changed_ignore_checksums(self):
         """Does the validate fail if a grade cell has changed with --invert and ignoring checksums?"""
@@ -71,7 +66,7 @@ class TestNbGraderValidate(BaseTestApp):
             "--invert",
             "--DisplayAutoGrades.ignore_checksums=True"
         ], stdout=True)
-        assert output.split(self.newline)[0] == "NOTEBOOK PASSED ON 2 CELL(S)!"
+        assert output.splitlines()[0] == "NOTEBOOK PASSED ON 2 CELL(S)!"
 
     def test_validate_unchanged_ignore_checksums(self):
         """Does the validation fail on an unchanged notebook with ignoring checksums?"""
@@ -80,13 +75,13 @@ class TestNbGraderValidate(BaseTestApp):
             "validate", "submitted-unchanged.ipynb",
             "--DisplayAutoGrades.ignore_checksums=True"
         ], stdout=True)
-        assert output.split(self.newline)[0] == "VALIDATION FAILED ON 1 CELL(S)! If you submit your assignment as it is, you WILL NOT"
+        assert output.splitlines()[0] == "VALIDATION FAILED ON 1 CELL(S)! If you submit your assignment as it is, you WILL NOT"
 
     def test_locked_cell_changed(self):
         """Does the validate fail if a locked cell has changed?"""
         self._copy_file(join("files", "submitted-locked-cell-changed.ipynb"), "submitted-locked-cell-changed.ipynb")
         output = run_nbgrader(["validate", "submitted-locked-cell-changed.ipynb"], stdout=True)
-        assert output.split(self.newline)[0] == "THE CONTENTS OF 2 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
+        assert output.splitlines()[0] == "THE CONTENTS OF 2 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
 
     def test_locked_cell_changed_ignore_checksums(self):
         """Does the validate pass if a locked cell has changed but we're ignoring checksums?"""
@@ -95,13 +90,13 @@ class TestNbGraderValidate(BaseTestApp):
             "validate", "submitted-locked-cell-changed.ipynb",
             "--DisplayAutoGrades.ignore_checksums=True"
         ], stdout=True)
-        assert output.split(self.newline)[0] == "VALIDATION FAILED ON 1 CELL(S)! If you submit your assignment as it is, you WILL NOT"
+        assert output.splitlines()[0] == "VALIDATION FAILED ON 1 CELL(S)! If you submit your assignment as it is, you WILL NOT"
 
     def test_invert_locked_cell_changed(self):
         """Does the validate fail if a locked cell has changed, even with --invert?"""
         self._copy_file(join("files", "submitted-locked-cell-changed.ipynb"), "submitted-locked-cell-changed.ipynb")
         output = run_nbgrader(["validate", "submitted-locked-cell-changed.ipynb", "--invert"], stdout=True)
-        assert output.split(self.newline)[0] == "THE CONTENTS OF 2 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
+        assert output.splitlines()[0] == "THE CONTENTS OF 2 TEST CELL(S) HAVE CHANGED! This might mean that even though the tests"
 
     def test_invert_locked_cell_changed_ignore_checksums(self):
         """Does the validate fail if a locked cell has changed with --invert and ignoring checksums?"""
@@ -111,7 +106,7 @@ class TestNbGraderValidate(BaseTestApp):
             "--invert",
             "--DisplayAutoGrades.ignore_checksums=True"
         ], stdout=True)
-        assert output.split(self.newline)[0] == "NOTEBOOK PASSED ON 1 CELL(S)!"
+        assert output.splitlines()[0] == "NOTEBOOK PASSED ON 1 CELL(S)!"
 
     def test_invert_locked_cell_changed_ignore_checksums_json(self):
         """Does the validate fail if a locked cell has changed with --invert and ignoring checksums?"""
