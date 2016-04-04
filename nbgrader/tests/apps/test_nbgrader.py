34c4478373c3f536605527297f7ea890866fa6cc
Jessica B. Hamrick <jhamrick@berkeley.edu>
2016-04-04 13:33:45 -0700

    Remove nbgrader.cmd from tests

--- nbgrader/tests/apps/test_nbgrader.py
+++ nbgrader/tests/apps/test_nbgrader.py
@@ -31,7 +31,7 @@ class TestNbGrader(BaseTestApp):
     def test_check_version(self, capfd):
         """Is the version the same regardless of how we run nbgrader?"""
         if sys.platform == 'win32':
-            out1 = "\r\n".join(run_command(["nbgrader.cmd", "--version"]).split("\r\n")[2:])
+            out1 = "\r\n".join(run_command(["nbgrader", "--version"]).split("\r\n")[2:])
         else:
             out1 = run_command(["nbgrader", "--version"])
         out2 = run_nbgrader(["--version"], stdout=True)
