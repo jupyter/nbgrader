e8964c1cbe1e0694f53b789f2496c3a84f6ec6e8
Holger Karl <hkarl@ieee.org>
2016-06-29 13:37:17 +0200

    need to update the checksum in the cell; it will be later corrected by overwritecells.preprocess_cell back to the original value

--- nbgrader/preprocessors/removehidden.py
+++ nbgrader/preprocessors/removehidden.py
@@ -8,6 +8,12 @@ class RemoveHidden(NbGraderPreprocessor):
 
     def preprocess_cell(self, cell, resources, cell_index):
 
-        cell.source = re.sub('START(?:.|\n)*?STOP', '', cell.source)
+        if utils.is_grade(cell) or utils.is_solution(cell) or utils.is_locked(cell):
+            cell.source = re.sub('START(?:.|\n)*?STOP', '', cell.source)
+
+            # we probably don't really need this? 
+            cell.metadata.nbgrader['oldchecksum'] = cell.metadata.nbgrader['checksum']
+            cell.metadata.nbgrader['checksum'] = utils.compute_checksum(cell)
+
 
         return cell, resources
