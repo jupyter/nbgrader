acb628aa16bfaa06361f8a01a777268d1ddabb51
Logan Page <page.lg@gmail.com>
2016-12-15 18:37:42 +0200

    ignore token auth for formgrade

--- nbgrader/tests/nbextensions/conftest.py
+++ nbgrader/tests/nbextensions/conftest.py
@@ -149,7 +149,7 @@ def nbserver(request, port, tempdir, coursedir, jupyter_config_dir, jupyter_data
     nbserver = sp.Popen([
         sys.executable, "-m", "jupyter", "notebook",
         "--no-browser",
-        "--NotebookApp.token=",
+        "--NotebookApp.token=''",  # Notebook >=4.3
         "--port", str(port)], **kwargs)
 
     def fin():
