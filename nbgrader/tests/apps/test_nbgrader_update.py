86227c6cbe7003ad39af1342fd5fc03d577675d5
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-01-14 11:12:22 -0800

    Add an additional nbgrader update test

--- nbgrader/tests/apps/test_nbgrader_update.py
+++ nbgrader/tests/apps/test_nbgrader_update.py
@@ -14,7 +14,7 @@ class TestNbGraderUpdate(BaseTestApp):
         """Is there an error if no arguments are given?"""
         run_nbgrader(["update"], retcode=1)
 
-    def test_update(self, db, course_dir):
+    def test_update_assign(self, db, course_dir):
         with open("nbgrader_config.py", "a") as fh:
             fh.write("""c.NbGrader.db_assignments = [dict(name='ps1', duedate='2015-02-02 14:58:23.948203 PST')]\n""")
             fh.write("""c.NbGrader.db_students = [dict(id="foo"), dict(id="bar")]""")
@@ -28,6 +28,14 @@ class TestNbGraderUpdate(BaseTestApp):
         # now assign should suceed
         run_nbgrader(["assign", "ps1", "--db", db])
 
+    def test_update_autograde(self, db, course_dir):
+        with open("nbgrader_config.py", "a") as fh:
+            fh.write("""c.NbGrader.db_assignments = [dict(name='ps1', duedate='2015-02-02 14:58:23.948203 PST')]\n""")
+            fh.write("""c.NbGrader.db_students = [dict(id="foo"), dict(id="bar")]""")
+
+        self._copy_file(join("files", "test.ipynb"), join(course_dir, "source", "ps1", "p1.ipynb"))
+        run_nbgrader(["assign", "ps1", "--db", db])
+
         # autograde should fail on old metadata, too
         self._copy_file(join("files", "test-v0.ipynb"), join(course_dir, "submitted", "foo", "ps1", "p1.ipynb"))
         run_nbgrader(["autograde", "ps1", "--db", db], retcode=1)
@@ -37,3 +45,22 @@ class TestNbGraderUpdate(BaseTestApp):
 
         # now autograde should suceed
         run_nbgrader(["autograde", "ps1", "--db", db])
+
+    def test_update_autograde_old_assign(self, db, course_dir):
+        with open("nbgrader_config.py", "a") as fh:
+            fh.write("""c.NbGrader.db_assignments = [dict(name='ps1', duedate='2015-02-02 14:58:23.948203 PST')]\n""")
+            fh.write("""c.NbGrader.db_students = [dict(id="foo"), dict(id="bar")]""")
+
+        self._copy_file(join("files", "test-v0.ipynb"), join(course_dir, "source", "ps1", "p1.ipynb"))
+        run_nbgrader(["assign", "ps1", "--db", db, "--CheckCellMetadata.enabled=False"])
+
+        # autograde should fail on old metadata, too
+        self._copy_file(join(course_dir, "release", "ps1", "p1.ipynb"), join(course_dir, "submitted", "foo", "ps1", "p1.ipynb"))
+        run_nbgrader(["autograde", "ps1", "--db", db], retcode=1)
+
+        # now update the metadata
+        run_nbgrader(["update", join(course_dir, "submitted")])
+
+        # now autograde should suceed
+        run_nbgrader(["autograde", "ps1", "--db", db])
+
