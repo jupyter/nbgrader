8728fedf29c4a0c4a51e7acc84bed5ca2de96fd8
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-07-20 14:47:30 -0400

    Bump version to 0.5.3.dev

--- nbgrader/_version.py
+++ nbgrader/_version.py
@@ -1,2 +1,2 @@
-version_info = (0, 5, 2)
+version_info = (0, 5, 3, 'dev')
 __version__ = '.'.join(map(str, version_info))
--- nbgrader/nbextensions/assignment_list/main.js
+++ nbgrader/nbextensions/assignment_list/main.js
@@ -6,7 +6,7 @@ define([
 ], function(Jupyter, $, utils, AssignmentList) {
     "use strict";
 
-    var nbgrader_version = "0.5.2";
+    var nbgrader_version = "0.5.3.dev";
 
     var ajax = utils.ajax || $.ajax;
     // Notebook v4.3.1 enabled xsrf so use notebooks ajax that includes the
