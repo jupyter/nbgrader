2e542f9c3467498a29ba7e1cedc466c34e23bc2e
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-05-31 14:55:42 +0100

    Extended support for filtering files copied in the exchange: fix outdated tests

--- nbgrader/tests/utils/test_utils.py
+++ nbgrader/tests/utils/test_utils.py
@@ -256,7 +256,7 @@ def test_compute_checksum_utf8():
     utils.compute_checksum(create_solution_cell("\u03b8", "markdown", "foo"))
     utils.compute_checksum(create_solution_cell(u'$$\\int^\u221e_0 x^2dx$$', "markdown", "foo"))
 
-def test_ignore(temp_cwd):
+def test_ignore_patterns(temp_cwd):
     dir = "foo"
     os.mkdir(dir)
     files = ["foo.txt", "long.txt", "truc.png"]
@@ -267,10 +267,10 @@ def test_ignore(temp_cwd):
                 fh.write("x"*2000)
     os.mkdir(join(dir,"foo"))
     files.append("foo")
-    assert utils.ignore(exclude=["foo*"])(dir, files) == ["foo.txt", "foo"]
-    assert utils.ignore(include=["*.txt"])(dir, files) == ["truc.png"]
-    assert utils.ignore(exclude=["foo.*"], include=["*.txt"])(dir, files) == ['foo.txt', 'truc.png']
-    assert utils.ignore(max_file_size=2)(dir, files) == ["long.txt"]
+    assert utils.ignore_patterns(exclude=["foo*"])(dir, files) == ["foo.txt", "foo"]
+    assert utils.ignore_patterns(include=["*.txt"])(dir, files) == ["truc.png"]
+    assert utils.ignore_patterns(exclude=["foo.*"], include=["*.txt"])(dir, files) == ['foo.txt', 'truc.png']
+    assert utils.ignore_patterns(max_file_size=2)(dir, files) == ["long.txt"]
 
 def test_is_ignored(temp_cwd):
     os.mkdir("foo")
