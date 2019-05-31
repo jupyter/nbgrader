5df600f5a3a149737636e0ed93c25c2e212ab141
Jessica B. Hamrick <jhamrick@berkeley.edu>
2019-05-31 10:23:11 +0100

    Do not add python 3.7 yet, as some dependencies do not yet support it

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup_args = dict(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     packages=find_packages(),
     package_data={
