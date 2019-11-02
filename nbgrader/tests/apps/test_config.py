4b7f233d8595c4548464bb72caf44feadf9d2666
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-11-02 11:04:46 +0100

    Added a comment about the test

--- nbgrader/tests/apps/test_config.py
+++ nbgrader/tests/apps/test_config.py
@@ -11,5 +11,7 @@ class TestCourseDirectory(BaseTestApp):
         config.Exchange.course_id = "abc101"
         config.CourseDirectory.root = "/root"
         coursedir = CourseDirectory(config=config)
+        # Test the support for both absolute paths and paths relative to the root directory
+        # See #1222
         assert coursedir.format_path("submitted", "alice", "HW1") == "/root/submitted/alice/HW1"
         assert coursedir.format_path("/bar/submitted", "alice", "HW1") == "/bar/submitted/alice/HW1"
