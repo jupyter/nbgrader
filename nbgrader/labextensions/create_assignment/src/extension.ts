853bc3af170ff46be70800965c449ae17582fe2c
Lawrence Lee <billee@ucdavis.edu>
2020-04-22 19:52:52 -0700

    Add data reading to create_assignment labextension
    Add side panel showing the nbgrader assignment data for the active
    notebook. At this point, it is read-only and does not automatically
    update upon change to the notebook's cell metadata.

---
+++ nbgrader/labextensions/create_assignment/src/extension.ts
@@ -0,0 +1,425 @@
+import {
+  Cell,
+  ICellModel
+} from '@jupyterlab/cells';
+
+import {
+  IChangedArgs
+} from '@jupyterlab/coreutils';
+
+import {
+  INotebookTracker,
+  Notebook,
+  NotebookPanel
+} from '@jupyterlab/notebook';
+
+import {
+  IObservableJSON,
+  IObservableMap
+} from '@jupyterlab/observables';
+
+import {
+  ReadonlyPartialJSONValue
+} from '@lumino/coreutils';
+
+import {
+  BoxPanel
+} from '@lumino/widgets';
+
+const CSS_CELL = 'nbgrader-Cell';
+const CSS_HEADER = 'nbgrader-CellHeader';
+const CSS_MOD_ACTIVE = 'nbgrader-mod-active';
+
+/**
+ * A widget which shows the "Create Assignment" widgets for the active notebook.
+ */
+export class CreateAssignmentWidget extends BoxPanel {
+  activeNotebook: NotebookPanel;
+  assignmentWidgets = new Map<NotebookPanel, NotebookWidget>();
+  notebookTracker: INotebookTracker;
+
+  constructor(tracker: INotebookTracker) {
+    super();
+    this.notebookTracker = tracker;
+    this.addClass('Create-Assignment-Widget');
+    this.addNotebookListeners(tracker);
+    this.activeNotebook = null;
+  }
+
+  addNotebookListeners(tracker: INotebookTracker) {
+    tracker.widgetAdded.connect(this.getNotebookAddedListener());
+    tracker.currentChanged.connect(this.getCurrentNotebookListener());
+  }
+
+  getCurrentNotebookListener(): (tracker: INotebookTracker, panel: NotebookPanel) => void {
+    return (tracker: INotebookTracker, panel: NotebookPanel) => {
+      if (this.activeNotebook != null) {
+        const widget = this.assignmentWidgets.get(this.activeNotebook);
+        if (widget != null) {
+          widget.hide();
+        }
+      }
+      if (panel != null) {
+        const widget = this.assignmentWidgets.get(panel)
+        if (widget != null) {
+          widget.show();
+        }
+      }
+      this.activeNotebook = panel;
+    }
+  }
+
+  getNotebookAddedListener(): (tracker: INotebookTracker, panel: NotebookPanel) => void {
+    return async (tracker: INotebookTracker, panel: NotebookPanel) => {
+      await panel.revealed
+      const notebookWidget = new NotebookWidget(panel);
+      this.addWidget(notebookWidget);
+      this.assignmentWidgets.set(panel, notebookWidget);
+      notebookWidget.disposed.connect(() => {
+        this.assignmentWidgets.delete(panel);
+      })
+      if (tracker.currentWidget != panel) {
+        notebookWidget.hide();
+      }
+    }
+  }
+}
+
+/**
+ * Dummy class for representing the nbgrader cell metadata.
+ */
+class NbgraderData {
+  grade: boolean;
+  grade_id: string;
+  locked: boolean;
+  points: number;
+  schema_version: number;
+  solution: boolean;
+  task: boolean;
+}
+
+/**
+ * Shows a cell's assignment data.
+ */
+class CellWidget extends BoxPanel {
+  cell: Cell;
+  _task: HTMLDivElement;
+  _gradeId: HTMLDivElement;
+  _points: HTMLDivElement;
+  _taskInput: HTMLSelectElement;
+  _gradeIdInput: HTMLInputElement;
+  _pointsInput: HTMLInputElement;
+
+  constructor(cell: Cell) {
+    super();
+    this.cell = cell;
+    this.addMetadataListener(cell);
+    this.initLayout();
+    this.initMetadata(cell);
+  }
+
+  async addMetadataListener(cell: Cell) {
+    await cell.ready;
+    cell.model.metadata.changed.connect(this.getMetadataChangedHandler());
+  }
+
+  getCellStateChangedListener(prompt: HTMLElement): (model: ICellModel, changedArgs:
+                                  IChangedArgs<any, any, string>) => void {
+    return (model: ICellModel, changedArgs: IChangedArgs<any, any, string>) => {
+      if (changedArgs.name == 'executionCount') {
+        const promptValue = changedArgs.newValue == null ? '' :
+          changedArgs.newValue;
+        prompt.innerText = `[${promptValue}]:`;
+      }
+    }
+  }
+
+  getGradeId(nbgraderData: NbgraderData): string {
+    if (nbgraderData == null || nbgraderData.grade_id == null) {
+      return 'cell-' + this._randomString(16);
+    }
+    return nbgraderData.grade_id;
+  }
+
+  getMetadataChangedHandler(): (metadata: IObservableJSON, changedArgs:
+                                IObservableMap.IChangedArgs<ReadonlyPartialJSONValue>) => void {
+    return (metadata: IObservableJSON, changedArgs: IObservableMap.IChangedArgs<ReadonlyPartialJSONValue>) => {
+      // TODO
+    }
+  }
+
+  getPoints(nbgraderData: NbgraderData): number {
+    if (nbgraderData == null) {
+      return 0;
+    }
+    return this._to_float(nbgraderData.points);
+  }
+
+  getSchemeaVersion(nbgraderData: NbgraderData): number {
+    if (nbgraderData === null) {
+      return 0;
+    }
+    return nbgraderData.schema_version;
+  }
+
+  initLayout() {
+    const headerElement = this.newHeaderElement();
+    const taskElement = this.newTaskElement();
+    const idElement = this.newIdElement();
+    const pointsElement = this.newPointsElement();
+    const elements = [headerElement, taskElement, idElement, pointsElement];
+    const fragment = document.createDocumentFragment();
+    for (const element of elements) {
+      fragment.appendChild(element);
+    }
+    this.node.appendChild(fragment);
+    this._task = taskElement;
+    this._gradeId = idElement;
+    this._points = pointsElement;
+    this._taskInput = taskElement.getElementsByTagName('select')[0];
+    this._gradeIdInput = idElement.getElementsByTagName('input')[0];
+    this._pointsInput = pointsElement.getElementsByTagName('input')[0];
+    this.addClass(CSS_CELL);
+  }
+
+  async initMetadata(cell: Cell) {
+    await cell.ready
+    if (cell.model == null) {
+      return;
+    }
+    this.updateValues(cell.model.metadata);
+  }
+
+  isGrade(nbgraderData: NbgraderData): boolean {
+    return nbgraderData != null && nbgraderData.grade == true;
+  }
+
+  isGraded(nbgraderData: NbgraderData): boolean {
+    return this.isGrade(nbgraderData) || this.isTask(nbgraderData);
+  }
+
+  isInvalid(nbgraderData: NbgraderData): boolean {
+    return !this.isTask(nbgraderData) && this.cell.model.type !== 'code' &&
+      (this.isSolution(nbgraderData) != this.isGrade(nbgraderData));
+  }
+
+  isLocked(nbgraderData: NbgraderData): boolean {
+    return !this.isSolution(nbgraderData) && (this.isGraded(nbgraderData) ||
+                                             nbgraderData.locked == true);
+  }
+
+  isSolution(nbgraderData: NbgraderData): boolean {
+    return nbgraderData != null && nbgraderData.solution == true;
+  }
+
+  isTask(nbgraderData: NbgraderData): boolean {
+    return nbgraderData != null && nbgraderData.task == true;
+  }
+
+  newHeaderElement(): HTMLDivElement {
+    const element = document.createElement('div');
+    element.className = CSS_HEADER;
+    element.style.width = 'max-content';
+    const promptNode =  this.cell.promptNode.cloneNode(true) as HTMLElement;
+    element.appendChild(promptNode);
+    this.cell.model.stateChanged.connect((model: ICellModel, args:
+                                          IChangedArgs<any>) => {
+                                           console.log(args);
+                                         });
+    this.cell.model.stateChanged.connect(this.getCellStateChangedListener(promptNode));
+    return element;
+  }
+
+  newIdElement(): HTMLDivElement {
+    const element = document.createElement('div');
+    const input = document.createElement('input');
+    input.type = 'text';
+    element.innerHTML = 'ID: '
+    element.appendChild(input);
+    return element;
+  }
+
+  newPointsElement(): HTMLDivElement {
+    const element = document.createElement('div');
+    const input = document.createElement('input');
+    input.type = 'number';
+    element.innerHTML = 'Points: ';
+    element.appendChild(input);
+    return element;
+  }
+
+  newTaskElement(): HTMLDivElement {
+    const element = document.createElement('div');
+    const select = document.createElement('select');
+    const options = new Map<string, string>([
+      ['', '-'],
+      ['manual', 'Manually graded answer'],
+      ['task', 'Manually graded task'],
+      ['solution', 'Autograded answer'],
+      ['tests', 'Autograded task'],
+      ['readonly', 'Read-only']
+    ]);
+    const fragment = document.createDocumentFragment();
+    for (const optionEntry of options.entries()) {
+      const option = document.createElement('option');
+      option.value = optionEntry[0];
+      option.innerHTML = optionEntry[1];
+      fragment.appendChild(option);
+    }
+    select.appendChild(fragment);
+    element.innerHTML = 'Type';
+    element.appendChild(select);
+    return element;
+  }
+
+  _randomString(length: number): string {
+    var result = '';
+    var chars = 'abcdef0123456789';
+    var i;
+    for (i = 0; i < length; i++) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
+  }
+
+  setActive(active: boolean) {
+    if (active) {
+      this.addClass(CSS_MOD_ACTIVE);
+    }
+    else {
+      this.removeClass(CSS_MOD_ACTIVE);
+    }
+  }
+
+  setGradeId(value: string) {
+    this._gradeIdInput.value = value;
+  }
+
+  setElementVisible(element: HTMLElement, visible: boolean) {
+    element.style.visibility = visible ? 'visible' : 'hidden';
+  }
+
+  setGradeIdVisible(visible: boolean) {
+    this.setElementVisible(this._gradeId, visible);
+  }
+
+  setPoints(value: number) {
+    this._pointsInput.value = value.toString();
+  }
+
+  setPointsVisible(visible: boolean) {
+    this.setElementVisible(this._points, visible);
+  }
+
+  setTask(value: string) {
+    this._taskInput.value = value;
+  }
+
+  _to_float(val: any): number {
+    if (val == null || val == '') {
+      return 0;
+    }
+    const valType = typeof(val);
+    if (valType == 'string') {
+      return parseFloat(val);
+    }
+    else if (valType == 'number') {
+      return val;
+    }
+    return 0;
+  }
+
+  updateValues(metadata: IObservableJSON) {
+    const nbgraderValue = metadata.get('nbgrader');
+    const nbgraderData = nbgraderValue == null ? null : nbgraderValue.valueOf() as NbgraderData;
+    if (nbgraderData == null) {
+      this.setTask('');
+      this.setGradeIdVisible(false);
+      this.setPointsVisible(false);
+      return;
+    }
+    if (nbgraderData.task) {
+      this.setTask('task');
+      this.setGradeId(nbgraderData.grade_id);
+      this.setPoints(nbgraderData.points);
+      this.setGradeIdVisible(true);
+      this.setPointsVisible(true);
+    } else if (nbgraderData.solution && nbgraderData.grade) {
+      this.setTask('manual');
+      this.setGradeId(nbgraderData.grade_id);
+      this.setPoints(nbgraderData.points);
+      this.setGradeIdVisible(true);
+      this.setPointsVisible(true);
+    } else if (nbgraderData.solution && this.cell.model.type === "code") {
+      this.setTask('solution');
+      this.setGradeId(nbgraderData.grade_id);
+      this.setGradeIdVisible(false);
+      this.setPointsVisible(true);
+    } else if (nbgraderData.grade && this.cell.model.type === "code") {
+      this.setTask('tests');
+      this.setGradeId(nbgraderData.grade_id);
+      this.setPoints(nbgraderData.points);
+      this.setGradeIdVisible(true);
+      this.setPointsVisible(true);
+    } else if (nbgraderData.locked) {
+      this.setTask('readonly');
+      this.setGradeId(nbgraderData.grade_id);
+      this.setPoints(nbgraderData.points);
+      this.setGradeIdVisible(true);
+      this.setPointsVisible(true);
+    } else {
+      this.setTask('');
+      this.setGradeId('');
+      this.setGradeIdVisible(false);
+      this.setPointsVisible(false);
+    }
+  }
+}
+
+/**
+ * Contains a notebook's "Create Assignment" UI.
+ */
+class NotebookWidget extends BoxPanel {
+  _activeCell = null as Cell;
+  cellWidgets = new Map<Cell, CellWidget>();
+
+  constructor(panel: NotebookPanel) {
+    super();
+    this._activeCell = panel.content.activeCell;
+    this.addClass('Assignment-Widget');
+    this.addCellListener(panel);
+    this.initCellWidgets(panel.content);
+    panel.disposed.connect(this.getNotebookDisposedListener());
+  }
+
+  addCellListener(panel: NotebookPanel) {
+    panel.content.activeCellChanged.connect(this.getActiveCellListener());
+  }
+
+  getActiveCellListener(): (notebook: Notebook, cell: Cell) => void {
+    return (notebook: Notebook, cell: Cell) => {
+      if (this._activeCell != null) {
+        this.cellWidgets.get(this._activeCell).setActive(false);
+      }
+      if (cell != null) {
+        this.cellWidgets.get(cell).setActive(true);
+      }
+      this._activeCell = cell;
+    }
+  }
+
+  getNotebookDisposedListener(): (panel: NotebookPanel) => void {
+    return (panel: NotebookPanel) => {
+      this.dispose();
+    }
+  }
+
+  initCellWidgets(notebook: Notebook) {
+    for (const cell of notebook.widgets) {
+      const cellWidget = new CellWidget(cell);
+      this.cellWidgets.set(cell, cellWidget);
+      this.addWidget(cellWidget);
+      cellWidget.setActive(notebook.activeCell === cell);
+    }
+  }
+}
--- nbgrader/labextensions/create_assignment/src/index.ts
+++ nbgrader/labextensions/create_assignment/src/index.ts
@@ -3,15 +3,36 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  INotebookTracker
+} from '@jupyterlab/notebook';
+
+import {
+  BoxPanel
+} from '@lumino/widgets';
+
+import {
+  CreateAssignmentWidget
+} from './extension';
+
 /**
  * Initialization data for the create_assignment extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'create-assignment',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension create-assignment is activated!');
-  }
+  requires: [INotebookTracker],
+  activate: activate_extension
 };
+
+function activate_extension(app: JupyterFrontEnd, tracker: INotebookTracker) {
+  console.log('Activating extension "create_assignment".');
+  const panel = new BoxPanel();
+  const createAssignmentWidget = new CreateAssignmentWidget(tracker);
+  panel.addWidget(createAssignmentWidget);
+  panel.id = 'nbgrader-create_assignemnt';
+  app.shell.add(panel, 'right');
+  console.log('Extension "create_assignment" activated.');
+}
 
 export default extension;
