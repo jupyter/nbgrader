7fcf2025799132753af42ab1ecf6c3aa72a29393
Abigail <aalmanza@ucdavis.edu>
2020-07-29 10:59:56 -0700

    fix bug

--- nbgrader/labextensions/assignment_list/assignment_list/__init__.py
+++ nbgrader/labextensions/assignment_list/assignment_list/__init__.py
@@ -16,5 +16,5 @@ def load_jupyter_server_extension(lab_app):
     lab_app: jupyterlab.labapp.LabApp
         JupyterLab application instance
     """
-    setup_handlers(lab_app.web_app)
+    setup_handlers(lab_app)
     lab_app.log.info("Registered HelloWorld extension at URL path /assignment_list")
