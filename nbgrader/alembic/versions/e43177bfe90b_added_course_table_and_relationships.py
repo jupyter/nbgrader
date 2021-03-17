6dd410e6bd2d11cd63f554a3558839845296cc68
Jessica B. Hamrick <jhamrick@berkeley.edu>
2021-03-17 00:19:55 +0000

    Fix test for table in alembic

--- nbgrader/alembic/versions/e43177bfe90b_added_course_table_and_relationships.py
+++ nbgrader/alembic/versions/e43177bfe90b_added_course_table_and_relationships.py
@@ -19,7 +19,7 @@ depends_on = None
 def _get_or_create_table(*args):
     ctx = op.get_context()
     con = op.get_bind()
-    table_exists = ctx.dialect.has_table(con.engine, args[0])
+    table_exists = ctx.dialect.has_table(con, args[0])
 
     if not table_exists:
         table = op.create_table(*args)
