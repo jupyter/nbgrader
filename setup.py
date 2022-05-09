ebdba407c4d95c07d93e4db1632a9e09b0897bb3
Nicolas Brichet <nicolas.brichet@inrae.fr>
2022-05-09 10:42:28 +0200

    Adds the setup.cfg file and cleans the setup.py file accordingly

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 # Distributed under the terms of the Modified BSD License.
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # get paths to all the extension files
 extension_files = []
@@ -49,28 +49,7 @@ for (dirname, dirnames, filenames) in os.walk("nbgrader/alembic"):
             continue
         alembic_files.append(os.path.join(root, filename))
 
-
-name = u'nbgrader'
-here = os.path.abspath(os.path.dirname(__file__))
-version_ns = {}
-with open(os.path.join(here, name, '_version.py')) as f:
-    exec(f.read(), {}, version_ns)
-
 setup_args = dict(
-    name=name,
-    version=version_ns['__version__'],
-    description='A system for assigning and grading notebooks',
-    author='Jupyter Development Team',
-    author_email='jupyter@googlegroups.com',
-    license='BSD',
-    url='https://github.com/jupyter/nbgrader',
-    keywords=['Notebooks', 'Grading', 'Homework'],
-    classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-    ],
-    packages=find_packages(),
     package_data={
         'nbgrader': extension_files + docs_files + alembic_files,
         'nbgrader.nbgraderformat': ["*.json"],
@@ -80,27 +59,7 @@ setup_args = dict(
             'nbextensions/files/*',
             'preprocessors/files/*'
         ]
-    },
-    entry_points={
-        'console_scripts': ['nbgrader=nbgrader.apps.nbgraderapp:main']
-    },
-    install_requires=[
-        "sqlalchemy>=1.4,<2",
-        "python-dateutil>=2.8",
-        "notebook>=6.4,<7",
-        "nbconvert>=6",
-        "requests>=2.26",
-        "jsonschema>=3",
-        "alembic>=1.7",
-        "rapidfuzz>=1.8",
-        "Jinja2>=3",
-        "MarkupSafe<2.2.0",
-        "jupyter_client<8",
-        "jupyter_server>=1.12",
-        "qtconsole>=5.2",
-        "ipywidgets>=7.6",
-        "nbclient>=0.6.1",
-    ]
+    }
 )
 
 if __name__ == "__main__":
