e2c294a82d3b3f116636c28d74f1330188c42bd8
Min RK <benjaminrk@gmail.com>
2022-06-10 11:44:44 +0200

    demo permssions for course_list
    - requires list:services (discovery), read:services (to get prefix)
    - requires server to inherit user permissions, so it can make these requests

--- demos/demo_one_class_multiple_graders/jupyterhub_config.py
+++ demos/demo_one_class_multiple_graders/jupyterhub_config.py
@@ -9,19 +9,38 @@ c.Authenticator.allowed_users = [
 
 # instructor1 and instructor2 have access to a shared server:
 c.JupyterHub.load_groups = {
-    'formgrader-course101': [
+    'formgrade-course101': [
         'instructor1',
         'instructor2',
     ]
 }
 c.JupyterHub.load_roles = [
     {
-        'name': 'formgrader-course101',
-        'groups': ['formgrader-course101'],
+        'name': 'formgrade-course101',
+        'groups': ['formgrade-course101'],
         'scopes': [
             'access:services!service=course101',
+            # access to the services API to discover the service(s)
+            'list:services',
+            f'read:services!service=course101',
         ],
-    }
+    },
+    # The class_list extension needs permission to access services
+    {
+        'name': 'server',
+        'scopes': [
+            'inherit',
+            # in JupyterHub 2.4, this can be a list of permissions
+            # greater than the owner and the result will be the intersection;
+            # until then, 'inherit' is the only way to have variable permissions
+            # for the server token by user
+            # "access:services",
+            # "list:services",
+            # "read:services",
+            # "users:activity!user",
+            # "access:servers!user",
+        ],
+    },
 ]
 
 # Start the notebook server as a service. The port can be whatever you want
