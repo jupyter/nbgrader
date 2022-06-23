ec3d328043ae161daf45094b902dbfaa5e53cfee
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-06-23 15:15:08 +0200

    Fix _version.py

--- nbgrader/_version.py
+++ nbgrader/_version.py
@@ -1,2 +1,2 @@
-version_info = (0, 8, 0, "dev")
-__version__ = '.'.join(map(str, version_info[:3])) + (("." + version_info[3]) if version_info[3] else "")
+version_info = (0, 8, 0, ".dev", "0")
+__version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
