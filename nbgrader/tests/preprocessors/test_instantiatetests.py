0e93295a422e541187857e982d19a64b42e0b3c7
Trevor Campbell <trevor.d.campbell@gmail.com>
2022-08-01 11:56:23 -0700

    fleshing out preproc tests for instantiate_tests

--- nbgrader/tests/preprocessors/test_instantiatetests.py
+++ nbgrader/tests/preprocessors/test_instantiatetests.py
@@ -14,10 +14,10 @@ def preprocessor():
 
 class TestInstantiateTests(BaseTestPreprocessor):
 
-    def test_load_test_template_file(self, preprocessor):
+    def test_load_default_test_template_file(self, preprocessor):
         resources = {
             'kernel_name': 'python3',
-            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/source/ps1'}
         }
         preprocessor._load_test_template_file(resources=resources)
         assert preprocessor.test_templates_by_type is not None
@@ -27,6 +27,16 @@ class TestInstantiateTests(BaseTestPreprocessor):
         assert preprocessor.check_template is not None
         assert preprocessor.normalize_template is not None
         assert preprocessor.setup_code is not None
+
+    def test_has_sanitizers(self, preprocessor):
+        assert 'python' in preprocessor.sanitizers.keys()
+        assert 'python3' in preprocessor.sanitizers.keys()
+        assert 'ir' in preprocessor.sanitizers.keys()
+
+    def test_has_comment_strs(self, preprocessor):
+        assert 'python' in preprocessor.comment_strs.keys()
+        assert 'python3' in preprocessor.comment_strs.keys()
+        assert 'ir' in preprocessor.comment_strs.keys()
 
     def test_replace_autotest_code(self, preprocessor):
         sol_cell = create_autotest_solution_cell()
@@ -44,12 +54,32 @@ class TestInstantiateTests(BaseTestPreprocessor):
         nb, resources = preprocessor.preprocess(nb, resources)
         assert 'assert' in nb['cells'][1]['source']
 
-    # TODO
-    # test some basic types (int, string, float, list)
-    # test error checking autotest
-    # test with different numbers of # strings and spaces at beginning
-    # test non-remove autotest directives in non-grading cells
-    # test for warning for autotest directive in nongrade cell
-    # test for error for autotest directive in nongrade cell
-    # test that improper statement raises error
+    # test that a warning is thrown when we set enforce_metadata = False and have an AUTOTEST directive in a non-grade cell
+    def test_warning_autotest_nongrade(self, preprocessor):
+        pass
+
+    # test that an error is thrown when we have an AUTOTEST directive in a non-grade cell
+    def test_error_autotest_nongrade(self, preprocessor):
+        pass
+
+    # test that AUTOTEST directives are not removed from non-grade cells (and that all non-grade cells are not modified)
+    def test_no_remove_autotest_nongrade(self, preprocessor):
+        pass
+
+    # test that invalid python statements in AUTOTEST directives cause errors
+    def test_error_bad_autotest_code(self, preprocessor):
+        pass
+
+    # test the code generated for some basic types; ensure correct solution gives success, a few wrong solns give failures
+    def test_int_autotest(self, preprocessor):
+        pass
+
+    def test_float_autotest(self, preprocessor):
+        pass
+
+    def test_string_autotest(self, preprocessor):
+        pass
+
+    def test_list_autotest(self, preprocessor):
+        pass
 
