ce46b0c7bc3740d4607aff3dba2dcd1ef6dc2ffe
Alireza Iranpour <alireza1iranpour@gmail.com>
2022-08-07 20:44:06 -0700

    Implemented additional tests

--- nbgrader/tests/preprocessors/test_instantiatetests.py
+++ nbgrader/tests/preprocessors/test_instantiatetests.py
@@ -5,6 +5,7 @@ from ...preprocessors import InstantiateTests
 from .base import BaseTestPreprocessor
 from .. import create_code_cell, create_text_cell, create_autotest_solution_cell, create_autotest_test_cell
 from nbformat.v4 import new_notebook
+from nbclient.client import NotebookClient
 
 
 @pytest.fixture
@@ -54,13 +55,44 @@ class TestInstantiateTests(BaseTestPreprocessor):
         nb, resources = preprocessor.preprocess(nb, resources)
         assert 'assert' in nb['cells'][1]['source']
 
-    # test that a warning is thrown when we set enforce_metadata = False and have an AUTOTEST directive in a non-grade cell
-    def test_warning_autotest_nongrade(self, preprocessor):
-        pass
+    # test that a warning is thrown when we set enforce_metadata = False and have an AUTOTEST directive in a
+    # non-grade cell
+    def test_warning_autotest_nongrade(self, preprocessor, caplog):
+        preprocessor.enforce_metadata = False
+        sol_cell = create_autotest_solution_cell()
+        test_cell = create_autotest_test_cell()
+        test_cell.metadata['nbgrader'] = {'grade': False}
+        nb = new_notebook()
+        nb.metadata['kernelspec'] = {
+            "name": "python3"
+        }
+        nb.cells.append(sol_cell)
+        nb.cells.append(test_cell)
+        resources = {
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+        }
+
+        nb, resources = preprocessor.preprocess(nb, resources)
+        assert "Autotest region detected in a non-grade cell; " in caplog.text
 
     # test that an error is thrown when we have an AUTOTEST directive in a non-grade cell
-    def test_error_autotest_nongrade(self, preprocessor):
-        pass
+    def test_error_autotest_nongrade(self, preprocessor, caplog):
+        sol_cell = create_autotest_solution_cell()
+        test_cell = create_autotest_test_cell()
+        test_cell.metadata['nbgrader'] = {'grade': False}
+        nb = new_notebook()
+        nb.metadata['kernelspec'] = {
+            "name": "python3"
+        }
+        nb.cells.append(sol_cell)
+        nb.cells.append(test_cell)
+        resources = {
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+        }
+        with pytest.raises(Exception):
+            nb, resources = preprocessor.preprocess(nb, resources)
+
+        assert "Autotest region detected in a non-grade cell; " in caplog.text
 
     # test that AUTOTEST directives are not removed from non-grade cells (and that all non-grade cells are not modified)
     def test_no_remove_autotest_nongrade(self, preprocessor):
@@ -68,18 +100,109 @@ class TestInstantiateTests(BaseTestPreprocessor):
 
     # test that invalid python statements in AUTOTEST directives cause errors
     def test_error_bad_autotest_code(self, preprocessor):
-        pass
+        sol_cell = create_autotest_solution_cell()
+        test_cell = create_autotest_test_cell()
+        test_cell.source = """
+            ### AUTOTEST length(answer)
+            """
+        test_cell.metadata['nbgrader'] = {'grade': True}
+        nb = new_notebook()
+        nb.metadata['kernelspec'] = {
+            "name": "python3"
+        }
+        nb.cells.append(sol_cell)
+        nb.cells.append(test_cell)
+        resources = {
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+        }
+        with pytest.raises(Exception):
+            nb, resources = preprocessor.preprocess(nb, resources)
 
-    # test the code generated for some basic types; ensure correct solution gives success, a few wrong solns give failures
+    # test the code generated for some basic types; ensure correct solution gives success, a few wrong solutions give
+    # failures
     def test_int_autotest(self, preprocessor):
-        pass
+        sol_cell = create_autotest_solution_cell()
+        sol_cell.source = """
+                        answer = 7
+                        """
+        test_cell = create_autotest_test_cell()
+
+        test_cell.metadata['nbgrader'] = {'grade': True}
+        nb = new_notebook()
+        nb.metadata['kernelspec'] = {
+            "name": "python3"
+        }
+        nb.cells.append(sol_cell)
+        nb.cells.append(test_cell)
+        resources = {
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+        }
+        nb, resources = preprocessor.preprocess(nb, resources)
+        executed_nb = NotebookClient(nb=nb).execute()
+        assert executed_nb['cells'][1]['outputs'][0]['text'] == 'Success!\n'
 
     def test_float_autotest(self, preprocessor):
-        pass
+        sol_cell = create_autotest_solution_cell()
+        sol_cell.source = """
+                        answer = 7.7
+                        """
+        test_cell = create_autotest_test_cell()
+
+        test_cell.metadata['nbgrader'] = {'grade': True}
+        nb = new_notebook()
+        nb.metadata['kernelspec'] = {
+            "name": "python3"
+        }
+        nb.cells.append(sol_cell)
+        nb.cells.append(test_cell)
+        resources = {
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+        }
+        nb, resources = preprocessor.preprocess(nb, resources)
+        executed_nb = NotebookClient(nb=nb).execute()
+        assert executed_nb['cells'][1]['outputs'][0]['text'] == 'Success!\n'
 
     def test_string_autotest(self, preprocessor):
-        pass
+        sol_cell = create_autotest_solution_cell()
+        sol_cell.source = """
+                        answer = 'seven'
+                        """
+        test_cell = create_autotest_test_cell()
+
+        test_cell.metadata['nbgrader'] = {'grade': True}
+        nb = new_notebook()
+        nb.metadata['kernelspec'] = {
+            "name": "python3"
+        }
+        nb.cells.append(sol_cell)
+        nb.cells.append(test_cell)
+        resources = {
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+        }
+        nb, resources = preprocessor.preprocess(nb, resources)
+        executed_nb = NotebookClient(nb=nb).execute()
+        assert executed_nb['cells'][1]['outputs'][0]['text'] == 'Success!\n'
 
     def test_list_autotest(self, preprocessor):
-        pass
+        sol_cell = create_autotest_solution_cell()
+        sol_cell.source = """
+                        answer = [1, 2, 3, 4, 5, 6, 7]
+                        """
+        test_cell = create_autotest_test_cell()
+
+        test_cell.metadata['nbgrader'] = {'grade': True}
+        nb = new_notebook()
+        nb.metadata['kernelspec'] = {
+            "name": "python3"
+        }
+        nb.cells.append(sol_cell)
+        nb.cells.append(test_cell)
+        resources = {
+            'metadata': {'path': 'nbgrader/docs/source/user_guide/'}
+        }
+        nb, resources = preprocessor.preprocess(nb, resources)
+        executed_nb = NotebookClient(nb=nb).execute()
+        assert executed_nb['cells'][1]['outputs'][0]['text'] == 'Success!\n'
+
+
 
