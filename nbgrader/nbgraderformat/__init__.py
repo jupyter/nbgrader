3192f85545b4c28f661951f01204ca4278f45edb
Tunç Başar Köse <tuncbasark@gmail.com>
2023-03-08 09:04:25 +0200

    Add new format with duplicate flag
    Changes in tests due to new format

--- nbgrader/nbgraderformat/__init__.py
+++ nbgrader/nbgraderformat/__init__.py
@@ -1,6 +1,6 @@
 from .common import ValidationError, SchemaTooOldError, SchemaTooNewError
-from .v3 import MetadataValidatorV3 as MetadataValidator
-from .v3 import read_v3 as read, write_v3 as write
-from .v3 import reads_v3 as reads, writes_v3 as writes
+from .v4 import MetadataValidatorV4 as MetadataValidator
+from .v4 import read_v4 as read, write_v4 as write
+from .v4 import reads_v4 as reads, writes_v4 as writes
 
 SCHEMA_VERSION = MetadataValidator.schema_version
