4c51c8174bacae4ecc6bcab32b02b0f288f793df
Violet Shreve <shreve@umich.edu>
2024-01-03 16:05:21 -0500

    Deprecate accessing Exchange classes directly from nbgrader.exchange

--- nbgrader/exchange/__init__.py
+++ nbgrader/exchange/__init__.py
@@ -1,21 +1,28 @@
-from nbgrader.exchange.abc import (Exchange, ExchangeError, ExchangeCollect, ExchangeFetch, ExchangeFetchAssignment,
-                                   ExchangeFetchFeedback, ExchangeList, ExchangeReleaseAssignment, ExchangeRelease,
-                                   ExchangeReleaseFeedback, ExchangeSubmit, ExchangeReleaseFeedback)
-from nbgrader.exchange import default
+import warnings
+from nbgrader.exchange.abc import  ExchangeError
+from nbgrader.exchange import abc, default
 from .exchange_factory import ExchangeFactory
 
+def __getattr__(name):
+    if name in abc.__all__:
+        warnings.warn(
+            f"Importing {name} from nbgrader.exchange is deprecated."
+            " Import from nbgrader.exchange.abc or the specific "
+            " exchange implementation instead.".format(name),
+            DeprecationWarning,
+            stacklevel=2
+        )
+
+    if hasattr(abc, name):
+        return getattr(abc, name)
+    elif hasattr(default, name):
+        return getattr(default, name)
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
 __all__ = [
-    "Exchange",
-    "ExchangeError",
-    "ExchangeCollect",
-    "ExchangeFetch",
-    "ExchangeFetchAssignment",
-    "ExchangeFetchFeedback",
-    "ExchangeList",
-    "ExchangeRelease",
-    "ExchangeReleaseAssignment",
-    "ExchangeReleaseFeedback",
-    "ExchangeSubmit",
+    "abc",
+    "default",
     "ExchangeFactory",
-    "default"
+    "ExchangeError",
 ]
